7e76e62fa0745eb3548ebbd974ddf60f8d286a91
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-07-03 19:20:53 +0530

    chore: remove console.log

--- libs/api/shared/src/lib/decorators/current-user.decorator.ts
+++ libs/api/shared/src/lib/decorators/current-user.decorator.ts
@@ -4,7 +4,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-    console.log({ user: ctx.getContext().req.user });
     return ctx.getContext().req.user;
   }
 );
--- libs/api/users/src/lib/users.resolver.ts
+++ libs/api/users/src/lib/users.resolver.ts
@@ -8,7 +8,6 @@ export class UsersResolver {
 
   @Query('me')
   me(@CurrentUser() user: CurrentUser) {
-    console.log(user);
     return this.userService.findById(user.id);
   }
 }
