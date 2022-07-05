a4e5bacb3f268e5944736dca413c54914778dc5b
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-07-06 00:04:21 +0530

    feat(ui): update data list styles

--- libs/ui/shared/src/lib/components/data-list.component.ts
+++ libs/ui/shared/src/lib/components/data-list.component.ts
@@ -1,17 +1,37 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { isNilPipe } from '../pipes';
 
 @Component({
   selector: 'show-off-data-list',
   template: `
-    <ul class="list-style-none">
-      <li *ngFor="let row of this.data" class="grid grid-cols-2">
-        <p class="text-slate-500">{{ row.label }}:</p>
-        <p class="text-slate-800">{{ row.value }}</p>
+    <ul class="list-style-none flex flex-col gap-1">
+      <li
+        *ngFor="let row of this.data"
+        class="grid grid-cols-2 items-center text-sm"
+      >
+        <ng-container *ngIf="!(row.value | isNil)">
+          <p class="text-xs text-slate-400">{{ row.label }}:</p>
+          <p class="text-slate-800">
+            <span
+              class="text-xs text-slate-500"
+              [class.font-mono]="row.type === 'number'"
+              >{{ row.prefix }}</span
+            >
+            <span [class.font-mono]="row.type === 'number'">{{
+              row.value
+            }}</span>
+            <span
+              class="text-xs text-slate-500"
+              [class.font-mono]="row.type === 'number'"
+              >{{ row.suffix }}</span
+            >
+          </p>
+        </ng-container>
       </li>
     </ul>
   `,
-  imports: [CommonModule],
+  imports: [CommonModule, isNilPipe],
   standalone: true,
 })
 export class DataListComponent {
@@ -21,5 +41,8 @@ export class DataListComponent {
 
 export interface DataListData {
   label: string;
-  value: string | number;
+  value: string | number | boolean | undefined;
+  type?: 'number' | 'string';
+  prefix?: string;
+  suffix?: string;
 }
