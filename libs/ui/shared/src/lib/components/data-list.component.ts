c942619ccbc3cb7752a24d664eacd61fc7bf9a09
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-07-08 23:35:12 +0530

    fix(data-list): remove empty rows

--- libs/ui/shared/src/lib/components/data-list.component.ts
+++ libs/ui/shared/src/lib/components/data-list.component.ts
@@ -1,33 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { isNilPipe } from '../pipes';
+import { isNil } from 'lodash-es';
 
 @Component({
   selector: 'show-off-data-list',
   template: `
     <ul class="list-style-none flex flex-col gap-1">
       <li
-        *ngFor="let row of this.data"
+        *ngFor="let row of this.formattedDataList"
         class="grid grid-cols-2 items-center text-sm"
       >
-        <ng-container *ngIf="!(row.value | isNil)">
-          <p class="text-xs text-slate-400">{{ row.label }}:</p>
-          <p class="text-slate-800">
-            <span
-              class="text-xs text-slate-500"
-              [class.font-mono]="row.type === 'number'"
-              >{{ row.prefix }}</span
-            >
-            <span [class.font-mono]="row.type === 'number'">{{
-              row.value
-            }}</span>
-            <span
-              class="text-xs text-slate-500"
-              [class.font-mono]="row.type === 'number'"
-              >{{ row.suffix }}</span
-            >
-          </p>
-        </ng-container>
+        <p class="text-xs text-slate-400">{{ row.label }}:</p>
+        <p class="text-slate-800">
+          <span
+            class="text-xs text-slate-500"
+            [class.font-mono]="row.type === 'number'"
+            >{{ row.prefix }}</span
+          >
+          <span [class.font-mono]="row.type === 'number'">{{ row.value }}</span>
+          <span
+            class="text-xs text-slate-500"
+            [class.font-mono]="row.type === 'number'"
+            >{{ row.suffix }}</span
+          >
+        </p>
       </li>
     </ul>
   `,
@@ -35,8 +32,12 @@ import { isNilPipe } from '../pipes';
   standalone: true,
 })
 export class DataListComponent {
+  formattedDataList: DataListData[] = [];
+
   @Input()
-  data: DataListData[] = [];
+  set data(data: DataListData[]) {
+    this.formattedDataList = data.filter((item) => !isNil(item.value));
+  }
 }
 
 export interface DataListData {
